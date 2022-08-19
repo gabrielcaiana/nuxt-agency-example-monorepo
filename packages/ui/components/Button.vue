87d875f65143ab2e5abedd5c718c889774d16e6c
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-08-18 23:22:37 -0300

    config vitest

---
+++ packages/ui/components/Button.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+  defineProps<{
+    disabled?: boolean;
+  }>()
+
+  defineEmits<{
+    (e: 'click:mybutton', value: string): void;
+  }>()
+</script>
+
+<template>
+  <button :disabled="disabled" @click="$emit('click:mybutton', 'hello from button')">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+  .button {
+    padding: 1rem;
+    border: none;
+    background-color: aquamarine;
+    color: black;
+    font-family: sans-serif;
+  }
+</style>
